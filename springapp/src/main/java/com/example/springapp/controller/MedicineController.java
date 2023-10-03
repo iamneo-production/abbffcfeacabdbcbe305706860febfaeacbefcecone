d6ef379be47dcc520b3da079a9af732967c58075
Iamneo <techadmin@iamneo.ai>
2023-10-03 06:05:23 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/example/springapp/controller/MedicineController.java
@@ -0,0 +1,33 @@
+package com.example.springapp.controller;
+
+import com.example.springapp.model.Medicine;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MedicineContoller {
+    private List<Medicine> medicineList = new ArrayList<>();
+
+    // POST endpoint to add a new medicine
+    @PostMapping("/")
+    public boolean addMedicine(@RequestBody Medicine medicine) {
+        return medicineList.add(medicine);
+    }
+
+    // PUT endpoint to update a medicine by medicineId
+    @PutMapping("/{id}") // Change the path variable to "id"
+    public Medicine updateMedicine(@PathVariable int id, @RequestBody Medicine updatedMedicine) {
+        for (Medicine medicine : medicineList) {
+            if (medicine.getMedicineId() == id) { // Update the parameter name to "id"
+                medicine.setMedicineName(updatedMedicine.getMedicineName());
+                medicine.setPrice(updatedMedicine.getPrice());
+                medicine.setQuantity(updatedMedicine.getQuantity());
+                medicine.setDescription(updatedMedicine.getDescription());
+                return medicine;
+            }
+        }
+        return null; // Handle not found scenario
+    }
+}
